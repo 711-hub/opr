a79777126843ad01092496a1806cb0751556e848
711-hub <projectsemalam711@gmail.com>
2025-10-02 06:35:28 +0700

    Update script.js

--- changedata/script.js
+++ changedata/script.js
@@ -4,16 +4,22 @@ function prosesData() {
   const userSet = new Set();
 
   lines.forEach(line => {
-    const parts = line.split("\t");
-    if (parts.length >= 6) {
-      const operator = parts[2].toLowerCase();
-      const keterangan = parts[4].trim().toLowerCase();
-      const userId = parts[5].trim();
-      if (keterangan.startsWith("bank :") && !operator.includes("ks")) {
+  const parts = line.split(/\t+/); // split tab atau tab ganda
+  if (parts.length >= 6) {
+    const operator = parts[2].toLowerCase();
+    const keterangan = parts[4].trim().toLowerCase();
+    const userId = parts[5].trim();
+
+    // filter operator ks atau vip
+    if (!operator.includes("ks") && !operator.includes("vip")) {
+      // filter keterangan bank : atau rek :
+      if (keterangan.includes("bank :") || keterangan.includes("rek :")) {
         userSet.add(userId);
       }
     }
-  });
+  }
+});
+
 
   const userList = Array.from(userSet);
   const output = userList.length > 0
